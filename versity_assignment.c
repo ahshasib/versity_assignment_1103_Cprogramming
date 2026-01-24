9481cab8121161ec9dbeee72a815f6740a658102
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-24 23:36:56 +0600

    solve problem no 3

--- versity_assignment.c
+++ versity_assignment.c
@@ -24,3 +24,19 @@ int main() {
         printf("%d is largest\n", b);
     return 0;
 }
+
+// ------------#problem no:3 ==> Find the largest of three numbers---------------
+
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if(a > b && a > c)
+        printf("%d is largest\n", a);
+    else if(b > c)
+        printf("%d is largest\n", b);
+    else
+        printf("%d is largest\n", c);
+    return 0;
+}

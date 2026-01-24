e0bf1e7289bd9df27bb80e184feb41e2a707df10
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-25 00:14:38 +0600

    solve problem number 7

--- versity_assignment.c
+++ versity_assignment.c
@@ -85,4 +85,19 @@ int main() {
     return 0;
 }
 
-// ------------#problem no:7 ==> Check whether a year is a leap year or not---------------
+// ------------#problem no:7 ==> Input a character and check if it's a vowel or consonant---------------
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("%c is a vowel\n", ch);
+    else
+        printf("%c is a consonant\n", ch);
+    return 0;
+}
+
+
+// ------------#problem no:8 ==> Input a character and check if it's a vowel or consonant---------------

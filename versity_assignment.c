ed0b8641f28b692e1dafd6182f633c87c02b2bc7
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-25 01:38:42 +0600

    solve problem number 14

--- versity_assignment.c
+++ versity_assignment.c
@@ -191,3 +191,18 @@ int main() {
     return 0;
 }
 
+
+// ------------#problem no:14 ==> Calculate the factorial of a number---------------
+
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    long long fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+        fact *= i;
+    printf("Factorial = %lld\n", fact);
+    return 0;
+}

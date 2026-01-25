b86d2293a1e879aea5a16db791158d8d2c21a619
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-26 00:19:13 +0600

    solve problem number 21

--- versity_assignment.c
+++ versity_assignment.c
@@ -311,3 +311,22 @@ int main() {
     printf("GCD = %d\n", gcd);
     return 0;
 }
+
+
+// ------------#problem no:21 ==> Find the LCM of two numbers---------------
+
+#include <stdio.h>
+int main() {
+    int a, b, max;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    max = (a > b) ? a : b;
+    while(1) {
+        if(max % a == 0 && max % b == 0) {
+            printf("LCM = %d\n", max);
+            break;
+        }
+        max++;
+    }
+    return 0;
+}

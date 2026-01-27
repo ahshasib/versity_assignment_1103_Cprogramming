2d22a8bb87fad01b001f43011678f3b3278ebe63
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-27 21:40:36 +0600

    solve problem number 31

--- versity_assignment.c
+++ versity_assignment.c
@@ -477,3 +477,21 @@ int main() {
     }
     return 0;
 }
+
+// ------------#problem no:31 ==>Check whether a number is perfect or not---------------
+
+#include <stdio.h>
+int main() {
+    int num, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(i = 1; i <= num/2; i++) {
+        if(num % i == 0)
+            sum += i;
+    }
+    if(sum == num)
+        printf("Perfect number\n");
+    else
+        printf("Not a perfect number\n");
+    return 0;
+}

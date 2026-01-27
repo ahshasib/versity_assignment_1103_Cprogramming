5de539349b9b3f47ccb2cdb9eb5f0db0117582f4
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-27 21:34:23 +0600

    solve problem number 26

--- versity_assignment.c
+++ versity_assignment.c
@@ -392,3 +392,20 @@ int main() {
     printf("Smallest digit = %d\n", min);
     return 0;
 }
+
+
+// ------------#problem no:26 ==> Find the largest digit in a number---------------
+
+#include <stdio.h>
+int main() {
+    int num, max = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while(num != 0) {
+        if(num % 10 > max)
+            max = num % 10;
+        num /= 10;
+    }
+    printf("Largest digit = %d\n", max);
+    return 0;
+}

34bce9d22f3a3c4041ce7098667b46129c7a91f9
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-27 21:31:01 +0600

    solve problem number 23

--- versity_assignment.c
+++ versity_assignment.c
@@ -349,3 +349,16 @@ int main() {
     printf("Even digits = %d\nOdd digits = %d\n", even, odd);
     return 0;
 }
+
+
+// ------------#problem no:23 ==> Print all even numbers between 1 and N---------------
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    for(i = 2; i <= n; i += 2)
+        printf("%d ", i);
+    return 0;
+}

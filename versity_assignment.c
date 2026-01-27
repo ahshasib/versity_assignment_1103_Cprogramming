66faa5f5c1362e4d4b6b8bc3bccf5cdd66868f41
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-27 21:32:11 +0600

    solve problem number 24

--- versity_assignment.c
+++ versity_assignment.c
@@ -362,3 +362,16 @@ int main() {
         printf("%d ", i);
     return 0;
 }
+
+
+// ------------#problem no:24 ==> Print all odd numbers between 1 and N---------------
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i += 2)
+        printf("%d ", i);
+    return 0;
+}

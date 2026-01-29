1a7c069f341a5a02747a874cfcbdcfa350503da0
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-29 18:43:27 +0600

    solve problem number 34

--- versity_assignment.c
+++ versity_assignment.c
@@ -526,3 +526,35 @@ int main() {
     printf("Sum of factors = %d\n", sum);
     return 0;
 }
+
+
+// ------------#problem no:34 ==>Check whether a number is a strong prime or not---------------
+
+#include <stdio.h>
+int isPrime(int n) {
+    int i;
+    if(n <= 1) return 0;
+    for(i = 2; i*i <= n; i++)
+        if(n % i == 0)
+            return 0;
+    return 1;
+}
+int main() {
+    int num, temp, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while(temp != 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    if(isPrime(num) && isPrime(sum))
+        printf("Strong prime number\n");
+    else
+        printf("Not a strong prime number\n");
+    return 0;
+}
+
+
+// ------------#problem no:35 ==>Check whether a number is a strong prime or not---------------
+

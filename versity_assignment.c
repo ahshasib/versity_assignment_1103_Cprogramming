7e8c139fe3c3473c12e0882dd8cd4ab5b0dc25cc
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-29 18:44:22 +0600

    solve problem number 35

--- versity_assignment.c
+++ versity_assignment.c
@@ -556,5 +556,26 @@ int main() {
 }
 
 
-// ------------#problem no:35 ==>Check whether a number is a strong prime or not---------------
+// ------------#problem no:35 ==>Count vowels and consonants in a string---------------
 
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, vowels = 0, consonants = 0;
+    printf("Enter a string: ");
+    gets(str);
+    for(i = 0; str[i] != '\0'; i++) {
+        if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')) {
+            if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+               str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+    printf("Vowels = %d\nConsonants = %d\n", vowels, consonants);
+    return 0;
+}
+
+
+// ------------#problem no:36 ==>Count vowels and consonants in a string---------------

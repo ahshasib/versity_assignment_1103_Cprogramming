e61d99993c511bb1e9795bda4e93239b9f95103b
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-29 18:47:05 +0600

    solve problem number 38

--- versity_assignment.c
+++ versity_assignment.c
@@ -612,4 +612,21 @@ int main() {
 }
 
 
-// ------------#problem no:38 ==>Print the reverse of a string using a loop---------------
+// ------------#problem no:38 ==>Find the second largest number among three numbers---------------
+
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if((a > b && a < c) || (a > c && a < b))
+        printf("Second largest = %d\n", a);
+    else if((b > a && b < c) || (b > c && b < a))
+        printf("Second largest = %d\n", b);
+    else
+        printf("Second largest = %d\n", c);
+    return 0;
+}
+
+
+// ------------#problem no:39 ==>Find the second largest number among three numbers---------------

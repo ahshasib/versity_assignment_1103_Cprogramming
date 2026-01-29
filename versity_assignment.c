ff14a71dc10257f69ae9d1262fd59525dc7de67d
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-29 18:51:07 +0600

    solve problem number 40

--- versity_assignment.c
+++ versity_assignment.c
@@ -646,3 +646,14 @@ int main() {
 }
 
 // ------------#problem no:40 ==>Print a number pattern (1, 12, 123, ...) using nested loops---------------
+
+#include <stdio.h>
+int main() {
+    int i, j;
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= i; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
